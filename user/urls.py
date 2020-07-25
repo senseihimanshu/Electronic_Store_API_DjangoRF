486b2adfbd3957e54b4930a6bc9d26bdfef4ccfe
senseihimanshu <hsharma151197@gmail.com>
2020-07-26 03:07:56 +0530

    Added token generation and authorizing request in api

--- user/urls.py
+++ user/urls.py
@@ -9,5 +9,6 @@ router.register('user', views.UserViewSet)
 
 urlpatterns = [
     # path('user/', views.UserView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
--- user/views.py
+++ user/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from user import serializers
 from user import models
@@ -36,3 +38,8 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = models.User.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnCredentials,)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
