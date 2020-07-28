3c3d8e5a18f85ef13cb931c646c30189b2011658
senseihimanshu <hsharma151197@gmail.com>
2020-07-29 05:09:50 +0530

    Configured backend for mysql

--- api/settings.py
+++ api/settings.py
@@ -87,8 +87,10 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'electronic_store',
+        'USER': 'root',
+        'PASSWORD': 'sensei'
     }
 }
 
